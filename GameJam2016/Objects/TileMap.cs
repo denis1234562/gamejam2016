67ad21674389ba8ee16e5e687060a9a3062b70a1
Danail Dimitrov <gaddas@abv.bg>
2016-01-30 18:02:39 +0200

    Map

--- GameJam2016/Objects/TileMap.cs
+++ GameJam2016/Objects/TileMap.cs
@@ -10,44 +10,60 @@ namespace GameJam2016.Objects
 {
     public class TileMap : IScene
     {
-        private Texture2D tileset;
+        public const float BOTTOM_OFFSET = 120f;
+        public const float WIDTH = 150f;
+        public const float HEIGHT = 100f;
+        public const float SPEED = 100f;
+
+        private float offset;
+        private Texture2D tile1;
+        private Texture2D tile2;
+        private string tile1filename;
+        private string tile2filename;
+        private string backgroundClass;
+
         public List<List<Tile>> Tiles;
 
         public TileMap(string mapFilename)
         {
-            ////using (var sr = new StreamReader(mapFilename))
-            ////{
-            ////    var backgroundClass = sr.ReadLine();
-            ////    var tilesFilename = sr.ReadLine();
-            ////    var indexX = 0;
-            ////    Tiles = new List<List<Tile>>();
+            using (var sr = new StreamReader(mapFilename))
+            {
+                backgroundClass = sr.ReadLine();
+                tile1filename = sr.ReadLine();
+                tile2filename = sr.ReadLine();
 
-            ////    while (!sr.EndOfStream)
-            ////    {
-            ////        var line = sr.ReadLine();
-            ////        if (line.StartsWith("-")) continue;
-            ////        if (string.IsNullOrWhiteSpace(line)) continue;
+                var indexX = 0;
+                Tiles = new List<List<Tile>>();
 
-            ////        Tiles[indexX] = new List<Tile>();
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line.StartsWith("-")) continue;
+                    if (string.IsNullOrWhiteSpace(line)) continue;
 
-            ////        var ss = line.ToCharArray();
+                    Tiles.Add(new List<Tile>());
 
-            ////        for (var i = 0; i < ss.Length; i++)
-            ////        {
-            ////            if (ss[i] == '.') continue;
+                    var ss = line.ToCharArray();
 
-            ////            var t = new Tile();
-            ////            t.Type = ss[i];
+                    for (var i = 0; i < ss.Length; i++)
+                    {
+                        if (ss[i] == '.') continue;
 
-            ////            Tiles[indexX].Add(t);                        
-            ////        }
-            ////    }
-            ////}
+                        var t = new Tile();
+                        t.Type = ss[i];
+
+                        Tiles[indexX].Add(t);
+                    }
+
+                    indexX++;
+                }
+            }
         }
 
         public void LoadContent(MyGame game)
         {
-            //tileset = game.Content.Load<Texture2D>(map.Tilesets[0].Name.ToString());
+            tile1 = game.Content.Load<Texture2D>(tile1filename);
+            tile2 = game.Content.Load<Texture2D>(tile2filename);
         }
 
         public void UnloadContent(MyGame game)
@@ -60,9 +76,60 @@ namespace GameJam2016.Objects
             throw new NotImplementedException();
         }
 
+        public void Update(MyGame game, GameTime gameTime, PlayerAction action)
+        {
+            //Get directional vector based on input
+            float direction = 0f;
+
+            if ((action & PlayerAction.MoveLeft) == PlayerAction.MoveLeft)
+            {
+                direction += -1f;
+            }
+            else if ((action & PlayerAction.MoveRight) == PlayerAction.MoveRight)
+            {
+                direction += 1f;
+            }
+
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            float distance = direction * SPEED * elapsed;
+            offset += distance;
+        }
+
         public void Draw(MyGame game, GameTime gameTime)
         {
-            throw new NotImplementedException();
+            var spriteBatch = game.spriteBatch;
+
+            spriteBatch.Begin();
+            for (var x = 0; x < this.Tiles.Count; x++)
+            {
+                var tileOffset = x * WIDTH - offset;
+
+                if (tileOffset < -WIDTH) continue;
+                if (tileOffset >= game.GraphicsDevice.Viewport.Width + WIDTH) continue;
+
+                var location = new Vector2(offset - tileOffset, 0);
+                for (var y = 0; y < this.Tiles[x].Count; y++)
+                {
+                    location.Y = game.GraphicsDevice.Viewport.Height - y * HEIGHT - BOTTOM_OFFSET;
+
+                    var tile = Tiles[x][y];
+                    var texture = getTextureByTileType(tile.Type);
+
+                    spriteBatch.Draw(texture, location, Color.White);
+                }
+            }
+
+            spriteBatch.End();
+        }
+
+        private Texture2D getTextureByTileType(char type)
+        {
+            if (type == ' ')
+            {
+                return tile2;
+            }
+
+            return tile1;
         }
     }
 }
